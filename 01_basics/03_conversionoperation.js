0432319e4520fd893d9963be2ab0f304b531c12f
nk369976 <nk369976@gmail.com>
2026-06-24 14:55:01 +0000

    operations in js

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,12 +1,12 @@
 let score="33abc"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 //"33abc" => NaN Not a Number
@@ -16,8 +16,8 @@ let isLoggedIn = 1
 
 let term = Boolean(isLoggedIn)
  
-console.log(typeof term);
-console.log(term);
+// console.log(typeof term);
+// console.log(term);
 
 // "" => false
 // "nikhil" => true
@@ -27,7 +27,46 @@ let someNumber = 32
 let stringNumber = String(someNumber)
 
 
-console.log(stringNumber);
+// console.log(stringNumber);
+
+
+
+// operations//
+
+let value = 3
+
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2**3); // 2 ki power 3
+// console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = " nikhil"
+
+let str3 = str1+str2
+
+
+
+
+console.log(+true);
+// console.log(true+);// this is wrong shows error
+
+
+let gameCounter = 100
+
+++gameCounter;
+
+console.log(gameCounter);
+
+
+
+
 
 
 
