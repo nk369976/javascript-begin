8c83a59410313bdc6ff720558496e50d6527118b
nk369976 <nk369976@gmail.com>
2026-07-07 14:16:02 +0000

    maths

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -5,3 +5,40 @@ console.log(name + repocount);
 
 console.log(`hello my name is ${name} and my repocount is ${repocount}`);
 
+const gameName = new String('nikhil')
+
+// console.log(gameName[0]);
+
+// console.log(gameName.__proto__);
+// 
+// console.log(gameName.length);
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('h'));
+
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8,3)
+console.log(anotherString);
+
+const newStringOne = "  nikhil  "
+
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://nikhil.com/kumar@20saroj"
+
+console.log(url.replace('@20','-'));
+
+console.log(url.includes('nikhil'));
+
+
+
+
+
+
+
+
+
---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,48 @@
+const score = 400
+console.log(score);
+
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.5342
+console.log(otherNumber.toPrecision(3));
+
+const secondNumber = 1000000
+
+console.log(secondNumber.toLocaleString('en-IN'));
+
+
+// +++++++++++++++++++++++++++maths+++++++++++++++++++++++++++
+
+
+console.log(Math);
+console.log(Math.abs(-5));
+console.log(Math.round(23.6));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.6));
+console.log(Math.min(3,5,7,9));
+console.log(Math.max(45,43,70,3));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min +1)) + min);
+
+
+
+
+
+
+
+
+
+
+
